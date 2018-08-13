a6531e529264c2aa98476842424bae218e837555
Messiah <Taylor.Jonathan@aol.com>
2018-08-13 19:53:24 -0400

    123

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,10 @@
-var one = "helloWord"
-var one = "helloWord"
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCZatYK5hPIJ2G5MuIvwKDh-yUWI3q_2vQ",
+    authDomain: "foodforthought-1f199.firebaseapp.com",
+    databaseURL: "https://foodforthought-1f199.firebaseio.com",
+    projectId: "foodforthought-1f199",
+    storageBucket: "foodforthought-1f199.appspot.com",
+    messagingSenderId: "907458220010"
+  };
+  firebase.initializeApp(config);
